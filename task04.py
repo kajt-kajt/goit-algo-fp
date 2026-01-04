af6a9d59fbcfbcf215aab50272409ed84a549429
kajt <kajt.kajt.kajt.kajt.kajt@gmail.com>
2026-01-04 23:05:17 +0200

    task 4

---
+++ task04.py
@@ -0,0 +1,65 @@
+"""
+Примітка. Суть завдання полягає у створенні дерева із купи.
+- Код виконується. Функція візуалізує бінарну купу.
+"""
+
+import uuid
+import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4()) # Унікальний ідентифікатор для кожного вузла
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val) # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)} # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+def draw_heap(heap):
+    if len(heap) < 1:
+        return
+    heap_tree = [ Node(elem) for elem in heap ]
+    n = len(heap)
+    for i in range(0, (n//2)):
+        if (2*i+1) < n:
+            heap_tree[i].left = heap_tree[2*i+1]
+        if (2*i+2) < n:
+            heap_tree[i].right = heap_tree[2*i+2]
+    draw_tree(heap_tree[0])
+
+
+# heap_array = [0, 4, 5, 10, 1, 3]
+# heapq.heapify(heap_array)
+# draw_heap(heap_array)
+
